aeb0be4b7fe9ccb0d81f546eb1cede0e1841a218
redcrjr25 <redmond50@gmail.com>
2025-05-09 08:37:23 -0400

    ch6 project additions

--- ch06/bulletPointAdder.py
+++ ch06/bulletPointAdder.py
@@ -10,4 +10,5 @@ lines = text.split("\n")
 for i in range(len(lines)):  # loop through all indexes in the "lines" list
     lines[i] = "* " + lines[i]  # add star to each string in "lines" list
 text = "\n".join(lines)
+print(text)  # Add this line to print result to terminal
 pyperclip.copy(text)
