31524b670630fa0f738ea186c8413bf24ddf6e46
redcrjr25 <redmond50@gmail.com>
2025-05-09 06:28:35 -0400

    ch 6 project addition

---
+++ ch06/bulletPointAdder.py
@@ -0,0 +1,13 @@
+# Project: Adding Bullets to Wiki Markup
+#! python3
+# bulletPointAdder.py - Adds Wikipedia bullet points to the start
+# of each line of text on the clipboard.
+import pyperclip # type: ignore
+
+text = pyperclip.paste()
+# Sepaarate lines and add stars.
+lines = text.split("\n")
+for i in range(len(lines)):  # loop through all indexes in the "lines" list
+    lines[i] = "* " + lines[i]  # add star to each string in "lines" list
+text = "\n".join(lines)
+pyperclip.copy(text)
