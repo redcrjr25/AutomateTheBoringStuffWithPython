0f02d0a3ed69be9a78ef24c98ee4b62129ccea0d
redcrjr25 <redmond50@gmail.com>
2025-05-19 13:38:00 -0400

    chapter 17 projects added

---
+++ ch17/countdown.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+# countdown.py - A simple countdown script.
+
+import time, subprocess
+
+timeLeft = 60
+while timeLeft > 0:
+    print(timeLeft, end="")
+    time.sleep(1)
+    timeLeft = timeLeft - 1
+
+# At the end of the countdown, play a sound file.
+subprocess.Popen(["open", "ch17/alarm.wav"])
